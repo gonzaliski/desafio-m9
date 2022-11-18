f91f73c518987c398304ef1b05c9087b31552baf
Gon <gon.mendez13@gmail.com>
2022-11-18 10:22:30 -0300

    middleware fix

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -1,22 +1,20 @@
+// pages/api/_middleware.ts
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  if (req.method == "OPTIONS") {
-    return new Response(null, {
-      status: 204,
-      headers: {
-        "Access-Control-Allow-Credentials": "true",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-        "Access-Control-Allow-Headers":
-          req.headers.get("Access-Control-Request-Headers") || "",
-        Vary: "Access-Control-Request-Headers",
-        "Content-Length": "0",
-      },
-    });
-  }
+   if (req.method == "OPTIONS") {
+      return new Response("", {
+         status: 204,
+         headers: {
+            "Access-Control-Allow-Credentials": "true",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+            "Access-Control-Allow-Headers": req.headers.get(
+               "Access-Control-Request-Headers"
+            ) as any,
+            Vary: "Access-Control-Request-Headers",
+            "Content-Length": "0",
+         },
+      });
+   }
 }
-
-export const config = {
-  matcher: ["/api/:path*"],
-};
